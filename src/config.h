7995ca1174f711ac9872b4bf4f8cc3f98cee9e86
Ekuty Barnabas Ebu <137317038+ekutyebu@users.noreply.github.com>
2026-06-03 00:33:53 +0100

    feat: add hardware and network configuration header file for VSMS project

--- src/config.h
+++ src/config.h
@@ -8,7 +8,7 @@
 // ==========================================
 #define WIFI_SSID "DarkDev"
 #define WIFI_PASSWORD "Man2001@"
-#define WIFI_IS_AP true // Set to true to start as Access Point, false to connect to local router
+#define WIFI_IS_AP false // Set to true to start as Access Point, false to connect to local router
 #define LOCAL_SSID "DarkDev"
 #define LOCAL_PASSWORD "Man2001@"
 #define BACKEND_SERVER_URL "https://vsms-6z4c.onrender.com/api/vitals" // Next.js backend API vital logs endpoint
